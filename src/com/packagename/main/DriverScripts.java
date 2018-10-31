05f7297c7f0b129b8e4210640a190469cb6e5f32
AmitS - DD <rohit0904singh@gmail.com>
2018-10-31 23:54:53 +0530

    all test suite excel updated

--- src/com/packagename/main/DriverScripts.java
+++ src/com/packagename/main/DriverScripts.java
@@ -23,7 +23,7 @@ public class DriverScripts {
 		APP_LOGS=Logger.getLogger("devpinoyLogger");
 		APP_LOGS.debug(" - Testing is Started!!");
     //Create the object for Suite File and get module names
-		Suitexls= new Xls_Reader("F:\\Automation-Test\\NewWorkspace_2\\HybridFramework_1\\src\\com\\packagename\\xls\\Suite.xlsx");
+		Suitexls= new Xls_Reader("F:\\Automation-Test\\NewWorkspace_2\\HybridFramework_1\\src\\com\\packagename\\xlsmain\\Suite.xlsx");
 		
 		int rowCount = Suitexls.getRowCount("TestSuite");
 		
@@ -35,7 +35,7 @@ public class DriverScripts {
 	                	tsiddm = Suitexls.getCellData("TestSuite", "TSID", row);
 	                	APP_LOGS.debug("Module to be executed as " +tsiddm );
 	                
-	                     modulexls = new Xls_Reader("F:\\Automation-Test\\NewWorkspace_2\\HybridFramework_1\\src\\com\\packagename\\xls\\"+tsiddm+".xlsx");
+	                     modulexls = new Xls_Reader("F:\\Automation-Test\\NewWorkspace_2\\HybridFramework_1\\src\\com\\packagename\\xlsmain\\"+tsiddm+".xlsx");
 	         
 	                      int rowexecuteCount = modulexls.getRowCount("TestCase");
 	                      // APP_LOGS.debug(rowexecuteCount);
